76ff1168169fc34bdc79e91224c4a62f42b64603
Jhonnier Sanchez <jhonnier.sanchez@gmail.com>
2017-08-19 12:06:04 -0500

    Formularios Validators - numeroPrimoAsync, diferencia errors y valid

--- app/formularioexp.component.js
+++ app/formularioexp.component.js
@@ -13,10 +13,11 @@ var forms_1 = require('@angular/forms');
 var FormularioexpComponent = (function () {
     function FormularioexpComponent() {
         this.formulario = new forms_1.FormGroup({
-            nombre: new forms_1.FormControl('Jaime', forms_1.Validators.required),
-            correo: new forms_1.FormControl('jhonnier.sanchez', [forms_1.Validators.required,
+            nombre: new forms_1.FormControl('Jhonnier', forms_1.Validators.required),
+            correo: new forms_1.FormControl('jhonnier@gmail.com', [forms_1.Validators.required,
                 forms_1.Validators.pattern('^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$'),
-                this.elCorreoNoEsUnico])
+                this.elCorreoNoEsUnico], this.elCorreoNoEsUnicoAsync),
+            numPrimo: new forms_1.FormControl('3', [forms_1.Validators.required], this.esNumeroPrimoAsync)
         });
     }
     FormularioexpComponent.prototype.enviarDatos = function () {
@@ -30,11 +31,47 @@ var FormularioexpComponent = (function () {
         }
         return null;
     };
+    //Validacion asincrona numero primo
+    FormularioexpComponent.prototype.esNumeroPrimoAsync = function (control) {
+        return new Promise(function (resolver, rechazar) {
+            setTimeout(function () {
+                var numPrimo = control.value;
+                var a = 0;
+                for (var i = 1; i < (numPrimo + 1); i++) {
+                    if (numPrimo % i == 0) {
+                        a++;
+                    }
+                }
+                if (a > 2) {
+                    console.log("No es Primo");
+                    resolver({ esNumeroPrimoAsync: true });
+                }
+                else {
+                    console.log("Si es Primo");
+                    resolver(null);
+                }
+            }, 10);
+        });
+    };
+    //Validacion personal
+    FormularioexpComponent.prototype.elCorreoNoEsUnicoAsync = function (control) {
+        //antes de que se vaya al codigo declaramos la promesa que nos permite hacer un llamado asincronico para reesolver o rechazar
+        //una peticion, entra a la funcion setTimeout y luego de 2 segundos ejecuta el codigo
+        return new Promise(function (resolver, rechazar) {
+            setTimeout(function () {
+                var correos = ['jhonnier@gmail.com', 'susana@gmail.com'];
+                if (correos.indexOf(control.value) != -1) {
+                    resolver({ elCorreoNoEsUnicoAsync: true });
+                }
+                resolver(null);
+            }, 2000);
+        });
+    };
     FormularioexpComponent = __decorate([
         core_1.Component({
             selector: 'formularioexp',
             templateUrl: './app/formularioexp.component.html',
-            styles: ["input.ng-invalid.ng-touched {\n        border-left: 3px solid red;\n        border-right: 3px solid red;\n     } \n     \n     input.ng-valid.ng-touched {\n        border-left: 3px solid green;\n        border-right: 3px solid green;\n     }"]
+            styles: ["input.ng-invalid {\n        border-left: 3px solid red;\n        border-right: 3px solid red;\n     } \n     \n     input.ng-valid {\n        border-left: 3px solid green;\n        border-right: 3px solid green;\n     }"]
         }), 
         __metadata('design:paramtypes', [])
     ], FormularioexpComponent);
--- app/formularioexp.component.ts
+++ app/formularioexp.component.ts
@@ -4,12 +4,12 @@ import {FormGroup, FormControl, Validators} from '@angular/forms'
 @Component({
     selector: 'formularioexp',
     templateUrl:'./app/formularioexp.component.html',
-    styles:[`input.ng-invalid.ng-touched {
+    styles:[`input.ng-invalid {
         border-left: 3px solid red;
         border-right: 3px solid red;
      } 
      
-     input.ng-valid.ng-touched {
+     input.ng-valid {
         border-left: 3px solid green;
         border-right: 3px solid green;
      }`]
@@ -20,10 +20,12 @@ export class FormularioexpComponent{
         formulario: FormGroup;
         constructor(){
               this.formulario=new FormGroup({
-                    nombre: new FormControl('Jaime',Validators.required),
-                    correo: new FormControl('jhonnier.sanchez',[Validators.required, 
+                    nombre: new FormControl('Jhonnier',Validators.required),
+                    correo: new FormControl('jhonnier@gmail.com',[Validators.required, 
                                                                 Validators.pattern('^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$'),
-                                                                this.elCorreoNoEsUnico])
+                                                                this.elCorreoNoEsUnico],
+                                                                this.elCorreoNoEsUnicoAsync),
+                    numPrimo: new FormControl('3',[Validators.required], this.esNumeroPrimoAsync)
               })  
         }
 
@@ -31,13 +33,54 @@ export class FormularioexpComponent{
             console.log(this.formulario);
         }
 
+       //Validacion personal
+        elCorreoNoEsUnico(control: FormControl): {[s:string]:Boolean}{
+                    var correos: string[] = ['jhonnier@gmail.com','susana@gmail.com'];
+                    if(correos.indexOf(control.value)!=-1){
+                        return {elCorreoNoEsUnico:true}
+                    }
+                return null;
+            }
+
+        //Validacion asincrona numero primo
+        esNumeroPrimoAsync(control: FormControl){
+            return new Promise((resolver, rechazar) =>
+            {
+               setTimeout(function(){
+                    var numPrimo = control.value;
+                    let a = 0; 
+                    for(let i = 1; i<(numPrimo + 1); i++){
+                        if(numPrimo%i==0){
+                            a++;
+                           }
+                        }
+                        if(a > 2){
+                             console.log("No es Primo");
+                             resolver ( {esNumeroPrimoAsync:true});
+                           
+                           }else{
+                             console.log("Si es Primo");
+                             resolver (null);
+                        }
+                    }, 10);
+               });
+            }
+
 
         //Validacion personal
-        elCorreoNoEsUnico(control: FormControl): {[s:string]:Boolean}{
-                var correos: string[] = ['jhonnier@gmail.com','susana@gmail.com'];
-                if(correos.indexOf(control.value)!=-1){
-                    return {elCorreoNoEsUnico:true}
-                }
-            return null;
+        elCorreoNoEsUnicoAsync(control: FormControl){
+            //antes de que se vaya al codigo declaramos la promesa que nos permite hacer un llamado asincronico para reesolver o rechazar
+            //una peticion, entra a la funcion setTimeout y luego de 2 segundos ejecuta el codigo
+            return new Promise((resolver, rechazar) =>
+            {
+               setTimeout(function(){
+                    var correos: string[] = ['jhonnier@gmail.com','susana@gmail.com'];
+                    if(correos.indexOf(control.value)!=-1){
+                        resolver ( {elCorreoNoEsUnicoAsync:true});
+                    }
+                    resolver (null);
+               }, 2000);
+            
+            });
         }
 }
